eb4d9d12fccb71d58d6b8691756a16322a4d8192
Arthur M <arthurmccomp@gmail.com>
2026-08-18 20:42:47 -0300

    Add files via upload

---
+++ lib/primeira_Pagina.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PrimeiraPagina extends StatelessWidget {
+  const PrimeiraPagina({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  
+}
